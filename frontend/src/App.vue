b4c0dcbd28676045d91a68a2db33253411dbed37
김종관 <colinder@naver.com>
2020-07-21 22:41:40 +0900

    :art: / edit_New Login Page / S03P12A307-3

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,6 +4,12 @@
   </div>
 </template>
 
+<script>
+export default {
+  name: "app"
+};
+</script>
+
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
--- frontend/src/views/accounts/Login.vue
+++ frontend/src/views/accounts/Login.vue
@@ -1,66 +1,79 @@
 <template>
-  <div>
-    <h1>
+  <b-container class="modalSize text-left">
+    <h1 class="mt-5 font-weight-bold">
       로그인을 하고 나면
       <br />좋은 일만 있을 거예요.
     </h1>
 
-    <div class="input-with-label">
-      <input
-        v-model="email"
-        v-bind:class="{error : error.email, complete:!error.email&&email.length!==0}"
-        @keyup.enter="Login"
-        id="email"
-        placeholder="이메일을 입력하세요."
-        type="text"
-      />
-      <label for="email">이메일</label>
-      <div class="error-text" v-if="error.email">{{error.email}}</div>
+    <div class="mt-3">
+      <b-input-group>
+        <template v-slot:prepend>
+          <b-input-group-text class="inputLabel">이메일</b-input-group-text>
+        </template>
+        <b-form-input
+          v-model="email"
+          v-bind:class="{error : error.email, complete:!error.email&&email.length!==0}"
+          @keyup.enter="Login"
+          id="email"
+          placeholder="이메일을 입력하세요."
+          type="text"
+        ></b-form-input>
+      </b-input-group>
     </div>
 
-    <div class="input-with-label">
-      <input
-        v-model="password"
-        type="password"
-        v-bind:class="{error : error.password, complete:!error.password&&password.length!==0}"
-        id="password"
-        @keyup.enter="Login"
-        placeholder="비밀번호를 입력하세요."
-      />
-      <label for="password">비밀번호</label>
-      <div class="error-text" v-if="error.password">{{error.password}}</div>
+    <div class="mt-3">
+      <b-input-group>
+        <template v-slot:prepend>
+          <b-input-group-text class="inputLabel">비밀번호</b-input-group-text>
+        </template>
+        <b-form-input
+          v-model="password"
+          type="password"
+          v-bind:class="{error : error.password, complete:!error.password&&password.length!==0}"
+          id="password"
+          @keyup.enter="Login"
+          placeholder="비밀번호를 입력하세요."
+        ></b-form-input>
+      </b-input-group>
     </div>
-    <button
-      class="btn btn--back btn--login"
-      @click="onLogin"
-      :disabled="!isSubmit"
-      :class="{disabled : !isSubmit}"
-    >로그인</button>
 
-    <div class="sns-login">
+    <!-- 로그인 버튼 -->
+    <div class="mt-3 text-center">
+      <button
+        class="btn"
+        @click="onLogin"
+        :disabled="!isSubmit"
+        :class="{disabled : !isSubmit}"
+      >로그인</button>
+    </div>
+
+    <hr>
+    <b-container fluid>
       <div class="text">
         <p>SNS 간편 로그인</p>
-        <div class="bar"></div>
       </div>
-
-      <kakaoLogin :component="component" />
-      <GoogleLogin :component="component" />
-    </div>
-    <div class="add-option">
+      <div class="d-flex justify-content-end sns-login">
+        <kakaoLogin  :component="component" />
+        <GoogleLogin :component="component" />
+      </div>
+    </b-container>
+    <hr>
+    
+    <b-container>
       <div class="text">
         <p>혹시</p>
-        <div class="bar"></div>
       </div>
-      <div class="wrap">
-        <p>비밀번호를 잊으셨나요?</p>
-        <!--<router-link to="/changePassword" class="btn--text">비밀번호 변경</router-link>-->
+      <div class="d-flex">
+        <p class="font-weight-bold ">비밀번호를 잊으셨나요?</p>
+        <router-link to="/accounts/changePassword">비밀번호 찾기</router-link>
       </div>
-      <div class="wrap">
-        <p>아직 회원이 아니신가요?</p>
-        <router-link to="/accounts/signin" class="btn--text">가입하기</router-link>
+      <div class="d-flex">
+        <p class="font-weight-bold">아직 회원이 아니신가요?</p>
+        <router-link to="/accounts/signin">가입하기</router-link>
       </div>
-    </div>
-  </div>
+    </b-container>
+    
+  </b-container>   
 </template>
 
 <script>
@@ -68,8 +81,8 @@ const SERVER_URL="http://localhost:8080/"
 import axios from 'axios'
 import PV from "password-validator";
 import * as EmailValidator from "email-validator";
-import KakaoLogin from "../../components/user/snsLogin/Kakao.vue";
-import GoogleLogin from "../../components/user/snsLogin/Google.vue";
+import KakaoLogin from "../../components/accounts/snsLogin/Kakao.vue";
+import GoogleLogin from "../../components/accounts/snsLogin/Google.vue";
 
 export default {
   components: {
@@ -159,3 +172,12 @@ export default {
 
 </script>
 
+<style scoped>
+.modalSize{
+  width: 570px;
+}
+.inputLabel{
+  width: 130px;
+}
+
+</style>
