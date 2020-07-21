6d7e5129b64e62a94778765955f89a5a10ca01aa
김지효 <ptoooool@naver.com>
2020-07-21 20:09:02 +0900

    :up: / navigation bar develop / S03p12A307-18

--- frontend/src/components/NavigationBar.vue
+++ frontend/src/components/NavigationBar.vue
@@ -1,15 +1,44 @@
 <template>
     <div class="nav-bar">
         <div class="header">
-            <button class="btn-home">굿 닥터</button>
-            <input type="text" class="input-search ml-4">
-			<button class="btn-search"><b-icon-search></b-icon-search></button>
-
-            <button class="btn-search-hospital">병원찾기<b-icon-caret-down-fill></b-icon-caret-down-fill></button>
-            <button class="btn-search-area">지역별찾기<b-icon-caret-down-fill></b-icon-caret-down-fill></button>
-
-            <button class="btn-write-review">리뷰쓰기</button>
-            <button class="btn-member"><b-icon-person-circle></b-icon-person-circle></button>
+            <div class="header-list">
+                <button class="btn-home" style="outline:0;">굿 닥터</button>
+                <input type="text" class="input-search ml-4">
+                <button class="btn-search" style="outline:0;"><b-icon-search></b-icon-search></button>
+            </div>
+            <div class="dropdown-list">
+                <div class="btn-hospital-list">
+                    <button class="btn-search-hospital" style="outline:0;">병원찾기<b-icon-caret-down-fill></b-icon-caret-down-fill></button>
+                    <div class="dropdown-content">
+                        <div class="drop-platform">
+                            <button class="dropdown-sub" ><img src="../assets/images/hospital/hospital1.png" alt=""><br>치과</button>
+                            <button class="dropdown-sub" ><img src="../assets/images/hospital/hospital2.png" alt=""><br>피부과</button>
+                            <button class="dropdown-sub" ><img src="../assets/images/hospital/hospital3.png" alt=""><br>성형외과</button>
+                            <button class="dropdown-sub" ><img src="../assets/images/hospital/hospital4.png" alt=""><br>안과</button>
+                            <button class="dropdown-sub" ><img src="../assets/images/hospital/hospital5.png" alt=""><br>산부인과</button>
+                            <button class="dropdown-sub" ><img src="../assets/images/hospital/hospital6.png" alt=""><br>비뇨기과</button>
+                            <button class="dropdown-sub" ><img src="../assets/images/hospital/hospital7.png" alt=""><br>정형외과</button>
+                            <button class="dropdown-sub" ><img src="../assets/images/hospital/hospital8.png" alt=""><br>마취통의학과</button>
+                            <button class="dropdown-sub" ><img src="../assets/images/hospital/hospital9.png" alt=""><br>신경외과</button>
+                            <button class="dropdown-sub" ><img src="../assets/images/hospital/hospital10.png" alt=""><br>재활의학과</button>
+                            <button class="dropdown-sub" ><img src="../assets/images/hospital/hospital11.png" alt=""><br>영상의학과</button>
+                            <button class="dropdown-sub" ><img src="../assets/images/hospital/hospital12.png" alt=""><br>외과</button>
+                            <button class="dropdown-sub" ><img src="../assets/images/hospital/hospital13.png" alt=""><br>신경과</button>
+                            <button class="dropdown-sub" ><img src="../assets/images/hospital/hospital14.png" alt=""><br>소아과</button>
+                            <button class="dropdown-sub" ><img src="../assets/images/hospital/hospital15.png" alt=""><br>내과</button>
+                            <button class="dropdown-sub" ><img src="../assets/images/hospital/hospital16.png" alt=""><br>이비인후과</button>
+                            <button class="dropdown-sub" ><img src="../assets/images/hospital/hospital17.png" alt=""><br>가정의학과</button>
+                            <button class="dropdown-sub" ><img src="../assets/images/hospital/hospital18.png" alt=""><br>한의원</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="btn-area-list">
+                    <button class="btn-search-area" style="outline:0;">지역별찾기<b-icon-caret-down-fill></b-icon-caret-down-fill></button>
+                    
+                </div>
+            </div>
+            <button class="btn-write-review" style="outline:0;">리뷰쓰기</button>
+            <button class="btn-member" style="outline:0;"><b-icon-person-circle></b-icon-person-circle></button>
         </div>
 
     </div>
