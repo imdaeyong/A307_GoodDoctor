fc420261793799598b7ea53bbb684fdffefccf44
김지효 <ptoooool@naver.com>
2020-07-22 15:03:42 +0900

    :up:/ navigation bar develop / S03P12A307-20

--- frontend/src/components/NavigationBar.vue
+++ frontend/src/components/NavigationBar.vue
@@ -2,7 +2,7 @@
     <div class="nav-bar">
         <div class="header">
             <div class="header-list">
-                <button class="btn-home" style="outline:0;">굿 닥터</button>
+                <button class="btn-home" style="outline:0;"><b-icon-heart-half></b-icon-heart-half> 굿 닥터</button>
                 <input type="text" class="input-search ml-4">
                 <button class="btn-search" style="outline:0;"><b-icon-search></b-icon-search></button>
             </div>
@@ -47,48 +47,125 @@
                             <button class="dropdown-sub-area" v-on:click="toggle('sejong')"><b-icon-geo-alt></b-icon-geo-alt>세종</button>
                         </div>
                         <div>
-                            <div class ="" v-if="area.seoul">
-                                <div><b-icon-geo-alt></b-icon-geo-alt>서울</div>
-                                강남구 강서구 송파구
+                            <div class ="dropdown-sub-gu" v-if="area[0].display">
+                                <div class ="area"><b-icon-geo-alt></b-icon-geo-alt>서울</div>
+                                <div class ="gu-list">
+                                    <div><a href="">강남구</a></div><div><a href="">강서구</a></div>
+                                    <div><a href="">중구</a></div><div><a href="">서초구</a></div><div><a href="">서초구</a></div><br>
+                                    <div><a href="">양천구</a></div><div><a href="">종로구</a></div>
+                                    <div><a href="">송파구</a></div><div><a href="">마포구</a></div><div><a href="">용산구</a></div><br>
+                                    <div><a href="">강동구</a></div><div><a href="">서대문구</a></div>
+                                    <div><a href="">성동구</a><br></div><div><a href="">관악구</a></div><div><a href="">은평구</a></div><br>
+                                    <div><a href="">동대문구</a></div><div><a href="">동작구</a></div>
+                                    <div><a href="">노원구</a></div><div><a href="">도봉구</a></div><div><a href="">영등포구</a></div><br>
+                                    <div><a href="">중랑구</a></div><div><a href="">강북구</a></div>
+                                    <div><a href="">금천구</a></div><div><a href="">광진구</a></div><div><a href="">성북구</a></div><br>
+                                    <div><a href="">구로구</a></div>
+                                </div>
+                                
                             </div>
-                            <div class ="" v-if="area.gyeonggido">
-                                <div><b-icon-geo-alt></b-icon-geo-alt>경기</div>
-                                강남구 강서구 송파구
+                            <div class ="dropdown-sub-gu" v-if="area[1].display">
+                                <div class ="area"><b-icon-geo-alt></b-icon-geo-alt>경기</div>
+                                <div class ="gu-list">
+                                    <div><a href="">수원</a></div><div><a href="">김포</a></div>
+                                    <div><a href="">고양</a></div><div><a href="">영통</a></div><div><a href="">용인</a></div><br>
+                                    <div><a href="">일산</a></div><div><a href="">성남</a></div>
+                                    <div><a href="">기흥</a></div><div><a href="">부천</a></div><div><a href="">분당</a></div><br>
+                                    <div><a href="">안성</a></div><div><a href="">남양주</a></div>
+                                    <div><a href="">과천</a></div><div><a href="">오산</a></div><div><a href="">동두천</a></div><br>
+                                    <div><a href="">광명</a></div><div><a href="">의왕</a></div>
+                                    <div><a href="">양주</a></div><div><a href="">안양</a></div><div><a href="">이천</a></div><br>
+                                    <div><a href="">의정부</a></div><div><a href="">시흥</a></div>
+                                    <div><a href="">평택</a></div><div><a href="">파주</a></div><div><a href="">안산</a></div><br>
+                                    <div><a href="">하남</a></div><div><a href="">포천</a></div>
+                                    <div><a href="">광주</a></div><div><a href="">화성</a></div><div><a href="">연천</a></div><br>
+                                    <div><a href="">군포</a></div><div><a href="">여주</a></div>
+                                    <div><a href="">가평</a></div><div><a href="">구리</a></div><div><a href="">양평</a></div>
+                                </div>
                             </div>
-                            <div class ="" v-if="area.incheon">
-                                <div><b-icon-geo-alt></b-icon-geo-alt>인천</div>
-                                강남구 강서구 송파구
+                            <div class ="dropdown-sub-gu" v-if="area[2].display">
+                                <div class ="area"><b-icon-geo-alt></b-icon-geo-alt>인천</div>
+                                <div class ="gu-list">
+                                    <div><a href="">송도</a></div><div><a href="">구월동</a></div>
+                                    <div><a href="">청라</a></div><div><a href="">연수구</a></div><div><a href="">남동구</a></div><br>
+                                    <div><a href="">서구</a></div><div><a href="">부평</a></div>
+                                    <div><a href="">미추홀구</a></div><div><a href="">계양구</a></div><div><a href="">중구</a></div><br>
+                                    <div><a href="">강화군</a></div><div><a href="">옹진군</a></div><div><a href="">동구</a></div>
+                                </div>
                             </div>
-                            <div class ="" v-if="area.busan">
-                                <div><b-icon-geo-alt></b-icon-geo-alt>부산</div>
-                                강남구 강서구 송파구
+                            <div class ="dropdown-sub-gu" v-if="area[3].display">
+                                <div class ="area"><b-icon-geo-alt></b-icon-geo-alt>부산</div>
+                                <div class ="gu-list">
+                                    <div><a href="">서면</a></div><div><a href="">남포동</a></div>
+                                    <div><a href="">광안리</a></div><div><a href="">해운대</a></div><div><a href="">센텀시티</a></div><br>
+                                    <div><a href="">송도</a></div><div><a href="">중구</a></div>
+                                    <div><a href="">남구</a></div><div><a href="">강서구</a></div><div><a href="">서구</a></div><br>
+                                    <div><a href="">북구</a></div><div><a href="">연제구</a></div>
+                                    <div><a href="">동구</a></div><div><a href="">동래구</a></div><div><a href="">수영구</a></div><br>
+                                    <div><a href="">영도구</a></div><div><a href="">사하구</a></div>
+                                    <div><a href="">사상구</a></div><div><a href="">부산진구</a></div><div><a href="">금정구</a></div><br>
+                                    <div><a href="">기장군</a></div>
+                                </div>
                             </div>
-                            <div class ="" v-if="area.daegu">
-                                <div><b-icon-geo-alt></b-icon-geo-alt>대구</div>
-                                강남구 강서구 송파구
+                            <div class ="dropdown-sub-gu" v-if="area[4].display">
+                                <div class ="area"><b-icon-geo-alt></b-icon-geo-alt>대구</div>
+                                <div class ="gu-list">
+                                    <div><a href="">동성로</a></div><div><a href="">달서구</a></div>
+                                    <div><a href="">수성구</a></div><div><a href="">중구</a></div><div><a href="">남구</a></div><br>
+                                    <div><a href="">서구</a></div><div><a href="">동구</a></div>
+                                    <div><a href="">북구</a></div><div><a href="">달성군</a></div>
+                                </div>
                             </div>
-                            <div class ="" v-if="area.gwangju">
-                                <div><b-icon-geo-alt></b-icon-geo-alt>광주</div>
-                                강남구 강서구 송파구
+                            <div class ="dropdown-sub-gu" v-if="area[5].display">
+                                <div class ="area"><b-icon-geo-alt></b-icon-geo-alt>광주</div>
+                                <div class ="gu-list">
+                                    <div><a href="">충장로</a></div><div><a href="">상무지구</a></div>
+                                    <div><a href="">동구</a></div><div><a href="">서구</a></div><div><a href="">북구</a></div><br>
+                                    <div><a href="">광산구</a></div><div><a href="">남구</a></div>
+                                </div>
                             </div>
-                            <div class ="" v-if="area.ulsan">
-                                <div><b-icon-geo-alt></b-icon-geo-alt>울산</div>
-                                강남구 강서구 송파구
+                            <div class ="dropdown-sub-gu" v-if="area[6].display">
+                                <div class ="area"><b-icon-geo-alt></b-icon-geo-alt>울산</div>
+                                <div class ="gu-list">
+                                    <div><a href="">삼산동</a></div><div><a href="">성남동</a></div>
+                                    <div><a href="">공업탑</a></div><div><a href="">중구</a></div><div><a href="">동구</a></div><br>
+                                    <div><a href="">울주군</a></div><div><a href="">남구</a></div>
+                                    <div><a href="">북구</a></div>
+                                </div>
                             </div>
-                            <div class ="" v-if="area.daejeon">
-                                <div><b-icon-geo-alt></b-icon-geo-alt>대전</div>
-                                강남구 강서구 송파구
+                            <div class ="dropdown-sub-gu" v-if="area[7].display">
+                                <div class ="area"><b-icon-geo-alt></b-icon-geo-alt>대전</div>
+                                <div class ="gu-list">
+                                    <div><a href="">둔산동</a></div><div><a href="">은행동</a></div>
+                                    <div><a href="">대덕구</a></div><div><a href="">동구</a></div><div><a href="">서구</a></div><br>
+                                    <div><a href="">유성구</a></div><div><a href="">중구</a></div>
+                                </div>
                             </div>
-                            <div class ="" v-if="area.sejong">
-                                <div><b-icon-geo-alt></b-icon-geo-alt>세종</div>
-                                강남구 강서구 송파구
+                            <div class ="dropdown-sub-gu" v-if="area[8].display">
+                                <div class ="area"><b-icon-geo-alt></b-icon-geo-alt>세종</div>
+                                <div class ="gu-list">
+                                    <div><a href="">도담동</a></div><div><a href="">나성동</a></div>
+                                    <div><a href="">새롬동</a></div><div><a href="">다정동</a></div><div><a href="">한솔동</a></div><br>
+                                    <div><a href="">보람동</a></div><div><a href="">가람동</a></div>
+                                    <div><a href="">어진동</a></div><div><a href="">소담동</a></div><div><a href="">반곡동</a></div><br>
+                                    <div><a href="">종촌동</a></div><div><a href="">대평동</a></div>
+                                    <div><a href="">아름동</a></div><div><a href="">고운동</a></div><div><a href="">조치원읍</a></div><br>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
+                <div class="btn-review-list">
+                    <button class="btn-write-review" style="outline:0;">리뷰쓰기</button>
+                </div>
+                <div class="btn-member-list">
+                    <button class="btn-member" style="outline:0;"><b-icon-person-circle></b-icon-person-circle>닉네임</button>
+                    <div class="dropdown-content">
+                        <div>회원정보</div>
+                        <div>로그아웃</div>
+                    </div>
+                </div>
             </div>
-            <button class="btn-write-review" style="outline:0;">리뷰쓰기</button>
-            <button class="btn-member" style="outline:0;"><b-icon-person-circle></b-icon-person-circle></button>
         </div>
     </div>
 </template>
@@ -100,27 +177,29 @@
                 console.log(city);
                 console.log(this.area.length);
                 for(var i = 0; i < this.area.length; i++){
-                    if(city == this.area[i].name && city == "seoul") this.area[i].seoul = !this.area[i].seoul;
+                    if(city == this.area[i].name) this.area[i].display = !this.area[i].display;
+                    else this.area[i].display = false;
                 }
-                
+                this.area.seoul = !this.area.seoul;
                 city = !city;
                 //this.area.seoul = !this.area.seoul;
             }
+
         },
         data: () => {
             return {
             area: [
-                {seoul: false, name: "seoul"},
-                {gyeonggido: false, name: "gyeonggido"},
-                {incheon: false, name: "incheon"},
-                {busan: false, name: "busan"},
-                {daegu: false, name: "daegu"},
-                {gwangju: false, name: "gwangju"},
-                {ulsan: false, name: "ulsan"},
-                {daejeon: false, name: "daejeon"},
-                {sejong: false, name: "sejong"}
-            ]
-
+                {display : false, name : "seoul"},
+                {display : false, name : "gyeonggido"},
+                {display : false, name : "incheon"},
+                {display : false, name : "busan"},
+                {display : false, name : "daegu"},
+                {display : false, name : "gwangju"},
+                {display : false, name : "ulsan"},
+                {display : false, name : "daejeon"},
+                {display : false, name : "sejong"}    
+            ],
+            memberBtn : false
             };
         }
     }
