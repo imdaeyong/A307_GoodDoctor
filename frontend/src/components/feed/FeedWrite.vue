9f4146a6c007f63eac7f6194cb14331ab73fc90e
김지효 <ptoooool@naver.com>
2020-07-29 15:23:15 +0900

    :new: / Feed Write Form Develop / S03P12A307-82

--- frontend/src/components/feed/FeedWrite.vue
+++ frontend/src/components/feed/FeedWrite.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
     <NavBar/>
+    <b-modal id="bv-modal-example" hide-footer hide-header no-close-on-backdrop no-close-on-esc>   
+      <User/>
+    </b-modal>
     <div>
 
 
@@ -10,19 +13,30 @@
           <div class="feed-wrap" v-bind:data="feed.data">
             <div class="feed-top">
               <img src= "../../assets/images/profile_default.png" alt="">
-              <div class="user-info">{{feed.user.nickname}} NEW !!!NEW !!!</div>
+              <div class="user-info">{{feed.user.nickname}} <span style="color : red; font-weight : bold"> NEW !!!NEW !!!</span> </div>
               <div class="user-hospital">{{feed.hospital.name}}<span>2020.07.13</span></div>
             </div>
-            <button>작성하기</button>
+            
+            <div class ="review-write-form">
+              <div v-if="openWrite == feed.id" class ="review-write">
+                <textarea name="" id="" cols="65%" rows="10">
+
+                </textarea>
+                <button v-on:click="addReview()">작성</button>
+              </div>
+            </div>
           </div>
-          
+          <div class ="review-write-btn">
+            <button v-if="openWrite != feed.id" v-on:click="feedWrite(feed.id)">작성하기</button>
+            <button v-if="openWrite == feed.id" v-on:click="feedWrite('')">작성닫기</button>
+          </div>
         </div>
         <div v-if="!feed.isNew">
           <div class="feed-wrap" v-bind:data="feed.data">
             <div class="feed-top">
               <img src= "../../assets/images/profile_default.png" alt="">
               <div class="user-info">{{feed.user.nickname}}</div>
-              <div class="user-hospital">{{feed.hospital.name}}<span>2020.07.13</span></div>
+              <div class="user-hospital">{{feed.hospital.name}}<span>{{feed.createDate}}</span></div>
             </div>
             <div class="feed-card">
               <img src= "../../assets/images/feed-sample.jpg" alt="">
@@ -59,13 +73,15 @@
 <script>
 const SERVER_URL="http://localhost:8080/"
 import NavBar from "../../components/NavigationBar.vue";
+import User from "../../views/accounts/Login.vue";
 import "../../assets/css/feedWrite.scss";
 import axios from "axios";
 import store from '@/vuex/store.js'
 export default {
-  components: { NavBar },
+  components: { NavBar, User },
   data: () => {
     return {
+      openWrite : "",
       feeds : [],
       nickname : "",
       isLogin : false,
@@ -74,15 +90,16 @@ export default {
     }
   },
    mounted(){
-    
-    this.nickname = store.state.userInfo.data.nickname;
-    this.isLogin = store.state.isLogin;
-    this.userId = store.state.userInfo.data.id
-    axios.get(`${SERVER_URL}feeds/${this.userId}`)
-    .then(data => {
-      this.feeds = data.data;
-      console.log(this.feeds);
-    })
+    if(!store.state.isLogin) this.$bvModal.show('bv-modal-example');
+    else{this.nickname = store.state.userInfo.data.nickname;
+      this.isLogin = store.state.isLogin;
+      this.userId = store.state.userInfo.data.id
+      axios.get(`${SERVER_URL}feeds/${this.userId}`)
+      .then(data => {
+        this.feeds = data.data;
+        console.log(this.feeds);
+      })
+    }
   },
   methods:{
     addLike(){ //좋아요 버튼 클릭시 실행 함수
@@ -109,8 +126,13 @@ export default {
       // .catch(err =>{
 
       // })
-      
-      
+    },
+    feedWrite(id){
+      //id를 받아서 펼치게 될 경우를 정해준다.
+      this.openWrite = id;
+    },
+    addReview(){
+
     }
   }
 }
